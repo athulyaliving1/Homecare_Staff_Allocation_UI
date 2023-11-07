c96a9d26207ec3ac8f64dd32934f2029b78d0e77
athulyaliving1 <muthukumar@athulyaliving.com>
2023-11-07 17:47:11 +0530

    FIX:Centos url fix

--- src/utilities/API_URL.jsx
+++ src/utilities/API_URL.jsx
@@ -1,3 +1,5 @@
 // export const API_URL = "http://localhost:4043"
 
-export const API_URL = "http://164.52.220.10:4046"
+// export const API_URL = "http://164.52.220.10:4046"
+
+export const API_URL = "http://124.123.68.117:4046"
