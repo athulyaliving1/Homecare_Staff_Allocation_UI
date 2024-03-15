f25d70af9937bff0e4237d3b60f035c62bb92a71
athulyaliving1 <110807506+athulyaliving1@users.noreply.github.com>
2024-03-15 17:22:42 +0530

    Update API_URL.jsx

--- src/utilities/API_URL.jsx
+++ src/utilities/API_URL.jsx
@@ -2,7 +2,10 @@
 
 // export const API_URL = "http://164.52.220.10:4046"
 
-export const API_URL = "http://124.123.68.117:4046"
+// export const API_URL = "http://124.123.68.117:4046"
+
+
+ export const API_URL = "https://homecare.athulyaliving.net"
 
 
 
